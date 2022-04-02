301aa390a2e695b9bbf32fb54eda6b007505e478
rholderried <roman_holderried@hotmail.com>
2022-04-02 18:26:59 +0200

    Tested functionality of the python SCI API

--- Python/testSCI.py
+++ Python/testSCI.py
@@ -5,26 +5,25 @@ par1 = Parameter(1, Datatype.DTYPE_UINT16)
 par2 = Parameter(2, Datatype.DTYPE_F32)
 
 fcn1 = Function(1, requestsUpstream=True)
+fcn2 = Function(2, argTypeList=[Datatype.DTYPE_UINT16], returnTypeList=[Datatype.DTYPE_F32]*10)
 
-inst = SCI('COM5', 128, baud = 2000000, numberFormat=NumberFormat.HEX)
+inst = SCI('COM3', 128, baud = 2000000, numberFormat=NumberFormat.HEX)
 
 start = time()
-#data = inst.getvalue(par1)
+inst.setvalue(par1, 6)
+data = inst.getvalue(par1)
+print(data)
 data = inst.requestUpstream(function=fcn1)
+print(data)
+data = inst.command(fcn2, paramList=[4])
+print(data)
+data = inst.getvalue(par1)
+print(data)
 print(time() - start)
 # packet.insert(0,2)
 # packet.append(3)
-
-
-# # msg = 'A>01020304050607'
-# # msg = 'A!DAT;3;567,A7,1'
-# # msgArr = bytearray(msg,'ASCII')
-# # msgArr.insert(0,2)
-# # msgArr.append(3)
-# print(packet)
 
 # # packet = inst._encode(cmd)
 # received = inst._decode(packet, SCI.CommandID.SETVAR)
 # testint = int(received.responseDesignator, 16)
 # testVar = inst._reinterpretDecodedIntToDtype(testint, SCI.Datatype.DTYPE_INT32)
-print(data)
